d9d4867d6386c553c6732cc4768b9af738d7bfc0
vanduyms <vanduyms@gmail.com>
2022-10-17 15:21:38 +0700

    remove private key of metamask

--- src/env.js
+++ src/env.js
@@ -3,5 +3,4 @@ export const env = {
     PINATA_SECRET_KEY: "eafa3a24300b85e682d7fc63b83ef148061eab2d1a3f3d87307ec21929b4637a",
     ALCHEMY_KEY: "https://eth-goerli.g.alchemy.com/v2/AXODOoFE9EUpK96e-jdHJgGIh3A5rtiM",
     API_KEY: "AXODOoFE9EUpK96e-jdHJgGIh3A5rtiM",
-    PRIVATE_KEY: "8835c78e03ba05daad187ed008d29d67daf9eaa26e9904f7ea0fc151f80d467d",
 }
